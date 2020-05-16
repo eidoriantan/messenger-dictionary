c7c41b35695edcefc74d041947b75a38b53812cb
Adriane Justine Tan <eidoriantan@gmail.com>
2020-05-16 20:14:22 +0800

    perf: Add greetings tool

---
+++ src/page/greeting.js
@@ -0,0 +1,31 @@
+
+const request = require('../utils/request.js')
+const getProof = require('../utils/proof.js')
+
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN
+const FB_ENDPOINT = 'https://graph.facebook.com/v7.0/me'
+
+/**
+ *  Setting the "Greeting" property with the text:
+ *
+ *    An English dictionary bot app built for Messenger
+ */
+async function sendGreeting () {
+  const params = new URLSearchParams()
+  params.set('access_token', ACCESS_TOKEN)
+  params.set('appsecret_proof', getProof())
+  const url = `${FB_ENDPOINT}/messenger_profile?${params.toString()}`
+  const data = {
+    greeting: [{
+      locale: 'default',
+      text: 'An English dictionary bot app built for Messenger'
+    }]
+  }
+
+  console.log('Setting greeting: ')
+  console.log(data)
+  const response = await request('POST', url, {}, data)
+  console.log(response)
+}
+
+sendGreeting()
